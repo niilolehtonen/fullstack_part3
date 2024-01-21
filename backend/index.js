1593a3fddfa460ce68ca2a8128ad737a04183a52
niilolehtonen <niilo.i.lehtonen@gmail.com>
2024-01-22 00:22:33 +0200

    more Render config

--- backend/index.js
+++ backend/index.js
@@ -95,7 +95,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
