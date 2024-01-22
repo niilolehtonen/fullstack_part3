d7e0413333ea6a48ae358c8678cc1b86ffee3d85
niilolehtonen <niilo.i.lehtonen@gmail.com>
2024-01-22 21:02:53 +0200

    3.13 - 3.14

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
@@ -14,35 +15,16 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
+const Person = require("./models/persons");
 
 const generateId = () => {
   return Math.floor(Math.random() * 1000) + 1;
 };
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get("/info", (req, res) => {
@@ -75,28 +57,23 @@ app.post("/api/persons", (req, res) => {
   const body = req.body;
 
   if (!body.name || !body.number) {
+    console.log("no name or number");
     return res.status(400).json({
-      error: "content missing",
+      error: "number or name missing",
     });
   }
 
-  if (persons.find((p) => p.name === body.name)) {
-    return res.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  };
+  });
 
-  persons = persons.concat(person);
-  res.json(person);
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
