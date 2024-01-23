dbdc267f53f2b28390874a6a672f7be46b209681
niilolehtonen <niilo.i.lehtonen@gmail.com>
2024-01-23 16:54:32 +0200

    fix

--- backend/models/persons.js
+++ backend/models/persons.js
@@ -15,8 +15,20 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+    validate: function (v) {
+      return /^(\d{2,3})-(\d{1,})$/.test(v);
+    },
+    message: (props) => `${props.value} is not a valid phone number!`,
+  },
 });
 
 personSchema.set("toJSON", {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -49,7 +49,8 @@ const App = () => {
             }, 5000)
           })
           .catch(error => {
-            setErrorMessage(`Information of ${person.name} has already been removed from the server`)
+            console.log(error.response.data.error)
+            setErrorMessage(`${error.response.data.error}`)
             setTimeout(() => {
               setErrorMessage(null)
             }, 5000)
